078c82072ddca4696d657df524f6ba6fa358828c
Bill Tang <billt4ng@gmail.com>
2018-03-15 17:26:22 -0700

    Increasing feature set and adjusting model

--- deeplearning.py
+++ deeplearning.py
@@ -7,15 +7,17 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras import optimizers
 import keras
+import threading
+import spacy
 
 from sklearn import preprocessing
 
 
 def clean(df):
   df['description'].fillna("NULLD", inplace=True)
-  df['category_lvl_1'] = df['category1'].fillna("NULLC1")
-  df['category_lvl_2'] = df['category2'].fillna("NULLC2")
-  df['category_lvl_3'] = df['category3'].fillna("NULLC3")
+  df['category1'] = df['category1'].fillna("NULLC1")
+  df['category2'] = df['category2'].fillna("NULLC2")
+  df['category3'] = df['category3'].fillna("NULLC3")
   df['product_type'] = df['product_type'].fillna("NULLPT")
   df['title'] = [x.lower() for x in df['title']]
   df['description'] = tqdm([bs(x, "html5lib").text.lower() for x in df['description']])
@@ -39,6 +41,7 @@ def generate_concise_features(df):
 
   data = []
   w2v = {}
+  nlp = spacy.load('en')
 
   with open("data/glove.6B/glove.6B.200d.txt", "rb") as lines:
     for line in tqdm(lines):
@@ -54,6 +57,41 @@ def generate_concise_features(df):
     cat2_words = str(row['category2']).split()
     cat3_words = str(row['category3']).split()
     sku_id = row['sku_id']
+    doc = nlp(row['title'])
+    #generate part of speech features
+    pos = []
+
+    for token in doc:
+      pos.append(token.pos_)
+    features.append(pos.count("DET"))
+    features.append(pos.count("ADJ"))
+    features.append(pos.count("ADP"))
+    features.append(pos.count("ADV"))
+    features.append(pos.count("AUX"))
+    features.append(pos.count("CONJ"))
+    features.append(pos.count("INTJ"))
+    features.append(pos.count("NOUN"))
+    features.append(pos.count("NUM"))
+    features.append(pos.count("PART"))
+    features.append(pos.count("PRON"))
+    features.append(pos.count("PROPN"))
+    features.append(pos.count("PUNCT"))
+    features.append(pos.count("SCONJ"))
+    features.append(pos.count("SYM"))
+    features.append(pos.count("VERB"))
+    features.append(pos.count("X"))
+
+    #generate noun chunk features
+    noun_roots = []
+    for chunk in doc.noun_chunks:
+      noun_roots.append(chunk.root.text)
+
+    noun_roots_embed = mean_embedding_vectorizer(noun_roots, w2v)[0]
+    for dim in noun_roots_embed:
+      features.append(dim)
+
+
+    #generate basic features
     features.append(len(row['title']))
     features.append(len(title_words))
     features.append(len(title_words) - len(set(title_words)))
@@ -63,6 +101,7 @@ def generate_concise_features(df):
     features.append(row['price'])
 
 
+    #generate glove embedding features
     title_embed = mean_embedding_vectorizer(title_words, w2v)[0]
 
     for dim in title_embed:
@@ -71,8 +110,19 @@ def generate_concise_features(df):
     if len(desc_words) > 0:
       desc_embed = mean_embedding_vectorizer(desc_words, w2v)[0]
       features.append(np.linalg.norm(title_embed - desc_embed))
+
+      desc_roots = []
+      doc = nlp(row["description"])
+      for chunk in doc.noun_chunks:
+        desc_roots.append(chunk.root.text)
+
+      desc_roots_embed = mean_embedding_vectorizer(desc_roots, w2v)[0]
+
+      features.append(np.linalg.norm(noun_roots_embed - desc_roots_embed))
+
     else:
       features.append(np.linalg.norm(title_embed))
+      features.append(np.linalg.norm(noun_roots_embed))
 
     if len(cat1_words) > 0:
       cat1_embed = mean_embedding_vectorizer(cat1_words, w2v)[0]
@@ -93,6 +143,7 @@ def generate_concise_features(df):
       features.append(np.linalg.norm(title_embed))
 
     data.append(features)
+
   return np.array(data)
 
 
@@ -138,12 +189,15 @@ if __name__ == "__main__":
     print("Generation complete")
     exit()
 
+  #normalize features
   concise_features = preprocessing.normalize(concise_features,axis=1)
   print(concise_features[0])
+
+  #Split out test set
   X_train = concise_features[:32000]
   X_test = concise_features[32000:]
-  y_train = keras.utils.to_categorical(df['clarity'][:32000].values, 2)
-  y_test = keras.utils.to_categorical(df['clarity'][32000:].values, 2)
+  y_train = keras.utils.to_categorical(df['conciseness'][:32000].values, 2)
+  y_test = keras.utils.to_categorical(df['conciseness'][32000:].values, 2)
 
   print(y_train)
 
@@ -151,9 +205,9 @@ if __name__ == "__main__":
   shape = concise_features.shape
   #Build keras model
   model = Sequential()
-
-  model.add(Dense(units=50, activation ='relu', input_dim=shape[1]))
-  model.add(Dense(units=20, activation='relu'))
+  model.add(Dense(units=400, activation ='relu', input_dim=shape[1]))
+  model.add(Dense(units=150, activation='relu'))
+  model.add(Dense(units=50, activation='relu'))
   model.add(Dense(units=2, activation = 'softmax'))
 
   sgd = optimizers.SGD(lr=0.01, decay=1e-7, momentum=0.9, nesterov=True)
@@ -161,10 +215,12 @@ if __name__ == "__main__":
                 optimizer=sgd,
                 metrics=['mse', 'accuracy'])
 
-  model.fit(X_train, y_train, epochs=6000, batch_size=128, validation_split=0.2)
+  #Train model
+  model.fit(X_train, y_train, epochs=2000, batch_size=128, validation_split=0.2)
 
   loss_and_metrics = model.evaluate(X_test, y_test, batch_size=128)
 
+  #Evaluate
   print("final_score", loss_and_metrics)
   print("shape", shape)
   classes = model.predict(X_test, batch_size=128)
